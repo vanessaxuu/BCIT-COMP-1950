6343b564652790114cefe240992ac48348650f42
Vanessa <vanessaxuu@gmail.com>
2018-03-17 00:34:12 -0700

    Added header/nav php and updated other html

---
+++ header.php
@@ -0,0 +1,18 @@
+    <div class="container"> 
+        <!-- Heading Row -->
+        <div class="row my-4">
+          <div class="col-lg-8">
+            <img class="img-fluid" src="http://bcbc.com/images/bcit%20logo.png" alt="BCIT BANNER">
+          </div>
+          <!-- /.col-lg-8 -->
+          <div class="col-lg-4 text-center">
+            <p>
+            <a class="btn btn-primary" href="https://www.bcit.ca/" target="_blank">BCIT</a>
+            <a class="btn btn-primary" href="https://learn.bcit.ca/d2l/home" target="_blank">D2L</a>
+            <a class="btn btn-primary" href="https://my.bcit.ca/cp/home/displaylogin" target="_blank">myBCIT</a>
+            </p>
+          </div>
+          <!-- /.col-md-4 -->
+        </div>
+        <!-- /.row -->
+    </div>
