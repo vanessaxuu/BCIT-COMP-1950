b5f9437da8ed6f8940e086b650295ade67485afd
Vanessa <vanessaxuu@gmail.com>
2018-03-20 00:41:50 -0700

    Quiz updated

--- php/lectures2_10.php
+++ php/lectures2_10.php
@@ -3,7 +3,7 @@
               <div class="row">
                 <div class="col-md-7">
                   <a href="http://bcitcomp.ca/1950/lecture/02/" target="_blank">
-                    <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                    <img class="img-fluid rounded mb-3 mb-md-0" src="images/html5.jpg" alt="HTML5">
                   </a>
                 </div>
                 <div class="col-md-5">
@@ -23,7 +23,7 @@
               <div class="row">
                   <div class="col-md-7">
                     <a href="http://bcitcomp.ca/1950/lecture/03/" target="_blank">
-                      <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                      <img class="img-fluid rounded mb-3 mb-md-0" src="images/advanced_css.jpg" alt="Advanced CSS">
                     </a>
                   </div>
                   <div class="col-md-5">
@@ -44,7 +44,7 @@
                 <div class="row">
                     <div class="col-md-7">
                       <a href="http://bcitcomp.ca/1950/lecture/04/" target="_blank">
-                        <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                        <img class="img-fluid rounded mb-3 mb-md-0" src="images/css_layout.jpg" alt="CSS Layouts">
                       </a>
                     </div>
                     <div class="col-md-5">
@@ -64,7 +64,7 @@
                   <div class="row">
                       <div class="col-md-7">
                         <a href="http://bcitcomp.ca/1950/lecture/05/" target="_blank">
-                          <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                          <img class="img-fluid rounded mb-3 mb-md-0" src="images/responsive_design.jpg" alt="Responsive Design">
                         </a>
                       </div>
                       <div class="col-md-5">
@@ -85,7 +85,7 @@
                 <div class="row">
                   <div class="col-md-7">
                     <a href="http://bcitcomp.ca/1950/lecture/06/" target="_blank">
-                            <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                            <img class="img-fluid rounded mb-3 mb-md-0" src="images/css3.jpg" alt="CSS3">
                           </a>
                         </div>
                         <div class="col-md-5">
@@ -106,7 +106,7 @@
               <div class="row">
                   <div class="col-md-7">
                     <a href="http://bcitcomp.ca/1950/lecture/07/" target="_blank">
-                      <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                      <img class="img-fluid rounded mb-3 mb-md-0" src="images/javascript_framework" alt="Javascript Frameworks">
                     </a>
                   </div>
                   <div class="col-md-5">
@@ -128,7 +128,7 @@
               <div class="row">
                   <div class="col-md-7">
                     <a href="http://bcitcomp.ca/1950/lecture/08/" target="_blank">
-                      <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                      <img class="img-fluid rounded mb-3 mb-md-0" src="images/server_side.jpg" alt="Server Side">
                     </a>
                   </div>
                   <div class="col-md-5">
@@ -148,7 +148,7 @@
                 <div class="row">
                     <div class="col-md-7">
                       <a href="http://bcitcomp.ca/1950/lecture/09/" target="_blank">
-                        <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                        <img class="img-fluid rounded mb-3 mb-md-0" src="images/template.jpg" alt="Templates">
                       </a>
                     </div>
                     <div class="col-md-5">
@@ -168,7 +168,7 @@
               <div class="row">
                   <div class="col-md-7">
                     <a href="http://bcitcomp.ca/1950/lecture/10/" target="_blank">
-                      <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                      <img class="img-fluid rounded mb-3 mb-md-0" src="images/methodology.jpg" alt="Methodology">
                     </a>
                   </div>
                   <div class="col-md-5">
--- php/quiz.php
+++ php/quiz.php
@@ -4,36 +4,62 @@
 <form>
     <p class="h4 text-center mb-4">Quiz #1</p>
 
-    <!-- Material input text -->
-    <div class="md-form">
-        <i class="fa fa-user prefix grey-text"></i>
-        <label for="materialFormContactNameEx">Name</label>
-        <input type="text" id="materialFormContactNameEx" class="form-control">
-    </div>
+        <div class="form-row">
+            <div class="col">
+            <input type="text" class="form-control" placeholder="First name">
+            </div>
+            <div class="col">
+            <input type="text" class="form-control" placeholder="Last name">
+            </div>
+        </div>
 
-    <!-- Material input email -->
-    <div class="md-form">
-        <i class="fa fa-envelope prefix grey-text"></i>
-        <input type="email" id="materialFormContactEmailEx" class="form-control">
-        <label for="materialFormContactEmailEx">Your email</label>
-    </div>
-    
-    <!-- Material input subject -->
-    <div class="md-form">
-        <i class="fa fa-tag prefix grey-text"></i>
-        <input type="text" id="materialFormContactSubjectEx" class="form-control">
-        <label for="materialFormContactSubjectEx">Subject</label>
-    </div>
-    
-    <!-- Material textarea message -->
-    <div class="md-form">
-        <i class="fa fa-pencil prefix grey-text"></i>
-        <textarea type="text" id="materialFormContactMessageEx" class="form-control md-textarea" rows="3"></textarea>
-        <label for="materialFormContactMessageEx">Your message</label>
-    </div>
+    <form class="form-inline">
+        <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Name one brand of HTTP server (1 mark):</label>
+            <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                <option selected>Choose...</option>
+                <option value="1">WAMP</option>
+                <option value="2">APACHE</option>
+                <option value="3">FILEZILLA</option>
+            </select>
+    </form>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Describe one advantage of implementing an error 404 page (2 marks):</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>  
 
-    <div class="text-center mt-4">
-        <button class="btn btn-outline-secondary" type="submit">Send<i class="fa fa-paper-plane-o ml-2"></i></button>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">The filename of configuring an Apache server is (1 mark):</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+            <option>apchie.ini</option>
+            <option>apache.conf</option>
+            <option>html.conf</option>
+            <option>.htaccess</option>
+            </select>
+        </div>
+
+        <label for="exampleRadios">In server-side scripting the conditional execution of code can be accomplished using (1 mark):</label>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+            <label class="form-check-label" for="exampleRadios1">
+                an if statement
+            </label>
+        </div>
+
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label" for="exampleRadios2">
+                a choice tree
+            </label>
+        </div>
+
+        <div class="form-check disabled">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
+            <label class="form-check-label" for="exampleRadios3">
+                forked code
+            </label>
+        </div>
+                
+    <button type="submit" class="btn btn-primary my-1">Submit</button>
     </div>
 </form>
 <!-- Material form contact -->
