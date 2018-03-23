48b0224a478b1e7c03a93b36a993b3f1234c2661
ahmadzeighami <36931896+ahmadzeighami@users.noreply.github.com>
2018-03-22 19:04:25 -0700

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,61 @@
+    <!-- Navigation -->
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <!-- SOCIAL MEDIA -->
+        <ul class="navbar-nav ml-auto socialmedia">
+          <li>
+              <a href="https://www.facebook.com/bcit.ca/" target="_blank"><img src="images/facebook.png" alt="BCIT Facebook"></a>
+          </li>
+          <li>
+              <a href="https://www.youtube.com/channel/UCF9kBFLsaPgIOHbrnC8DnmA" target="_blank"><img src="images/youtube.png" alt="BCIT Youtube"></a>
+          </li>
+          <li>
+              <a href="https://twitter.com/bcit?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><img src="images/twitter.png" alt="BCIT Twitter"></a>
+          </li>
+          <li>
+              <a href="https://www.instagram.com/lifeatbcit/?hl=en" target="_blank"><img src="images/instagram.png" alt="BCIT Instagram"></a>
+          </li>
+        </ul>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <!-- PUT "active" class for current link -->
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home
+                <!-- Note: To display current page
+                <span class="sr-only">(current)</span>
+                -->
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="http://bcitcomp.ca/1950/resources/" target="_blank">Resources</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Lectures</a>
+              <div class="dropdown-content">
+              <a href="lecture01.html" target="_blank">01: Tools &amp; Standards</a>
+              <a href="http://bcitcomp.ca/1950/lecture/02/" target="_blank">02: HTML5</a>
+              <a href="http://bcitcomp.ca/1950/lecture/03/" target="_blank">03: Advanced CSS</a>
+              <a href="http://bcitcomp.ca/1950/lecture/04/" target="_blank">04: CSS Layouts</a>
+              <a href="http://bcitcomp.ca/1950/lecture/05/" target="_blank">05: Responsive Design</a>
+              <a href="http://bcitcomp.ca/1950/lecture/06/" target="_blank">06: CSS3</a>
+              <a href="http://bcitcomp.ca/1950/lecture/07/" target="_blank">07: Javascript Frameworks</a>
+              <a href="http://bcitcomp.ca/1950/lecture/08/" target="_blank">08: Server Architecture</a>
+              <a href="http://bcitcomp.ca/1950/lecture/09/" target="_blank">09: Templates, Tools</a>
+              <a href="http://bcitcomp.ca/1950/lecture/10/" target="_blank">10: Methodology</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="quiz.html">Quiz</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="http://bcitcomp.ca/1950/about/" target="_blank">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
